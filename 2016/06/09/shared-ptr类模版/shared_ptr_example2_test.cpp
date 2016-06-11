381e249c7cb2cff4dd9f1dc681c3111d682e3417
deyituo <tuodeyi@mail2.sysu.edu.cn>
2016-06-11 17:00:01 +0800

    Site updated: 2016-06-11 17:00:01

---
+++ 2016/06/09/shared-ptr类模版/shared_ptr_example2_test.cpp
@@ -0,0 +1,13 @@
+#include "shared_ptr_example2.hpp"
+
+int main()
+{
+  example a;
+  a.do_something();
+  example b(a);
+  b.do_something();
+  example c;
+  c = a;
+  c.do_something();
+  return 0;
+}
